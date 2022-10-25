48963aad991103a7eefbbf1988f7c992a3af3306
k-okawa <kyohei.okawa@10antz.co.jp>
2022-10-25 18:05:17 +0900

    pass exception test

--- Assets/Bg/UniTaskObjectPoolTest/EditModeTest/ObjectPoolTest.cs
+++ Assets/Bg/UniTaskObjectPoolTest/EditModeTest/ObjectPoolTest.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using Bg.UniTaskObjectPool;
 using Cysharp.Threading.Tasks;
 using NUnit.Framework;
 using UnityEngine.TestTools;
+using Assert = UnityEngine.Assertions.Assert;
 
 namespace Bg.UniTaskObjectPoolTest
 {
@@ -25,6 +27,33 @@ namespace Bg.UniTaskObjectPoolTest
                     CommonTest.Destroy
                 );
             await CommonTest.NormalTest(objectPool);
+
+            objectPool = new AsyncObjectPool<CommonTest.SamplePoolObj>(
+                CommonTest.CreateFunc
+            );
+            await CommonTest.NormalTest(objectPool);
+            
+            objectPool = new AsyncObjectPool<CommonTest.SamplePoolObj>(
+                CommonTest.CreateFunc,
+                CommonTest.OnGet
+            );
+            await CommonTest.NormalTest(objectPool);
+            
+            objectPool = new AsyncObjectPool<CommonTest.SamplePoolObj>(
+                CommonTest.CreateFunc,
+                CommonTest.OnGet,
+                CommonTest.OnRelease
+            );
+            await CommonTest.NormalTest(objectPool);
+            
+            objectPool = new AsyncObjectPool<CommonTest.SamplePoolObj>(
+                CommonTest.CreateFunc,
+                CommonTest.OnGet,
+                CommonTest.OnRelease,
+                CommonTest.Destroy,
+                true, 0, 2
+            );
+            await CommonTest.NormalTest(objectPool);
         });
         
         [UnityTest]
@@ -37,6 +66,49 @@ namespace Bg.UniTaskObjectPoolTest
                 CommonTest.Destroy
             );
             await CommonTest.LoopTest(objectPool, 10);
+        });
+        
+        [UnityTest]
+        public IEnumerator ExceptionTest() => UniTask.ToCoroutine(async () =>
+        {
+            bool hasException = false;
+            try
+            {
+                var objectPool = new AsyncObjectPool<CommonTest.SamplePoolObj>(null);
+                await CommonTest.LoopTest(objectPool, 10);
+            }
+            catch (ArgumentNullException)
+            {
+                hasException = true;
+            }
+            Assert.IsTrue(hasException);
+            hasException = false;
+            
+            try
+            {
+                var objectPool = new AsyncObjectPool<CommonTest.SamplePoolObj>(CommonTest.CreateFunc, maxSize: 0);
+                await CommonTest.LoopTest(objectPool, 10);
+            }
+            catch (ArgumentException)
+            {
+                hasException = true;
+            }
+            Assert.IsTrue(hasException);
+            hasException = false;
+            
+            try
+            {
+                var objectPool = new AsyncObjectPool<CommonTest.SamplePoolObj>(CommonTest.CreateFunc);
+                var obj = await objectPool.Get();
+                await objectPool.Release(obj);
+                await objectPool.Release(obj);
+            }
+            catch (InvalidOperationException)
+            {
+                hasException = true;
+            }
+            Assert.IsTrue(hasException);
+            hasException = false;
         });
     }
 }
